179e1f7e7786506d0558f5b19aa52770f2a91dc9
divakratya <64886481+divakratya@users.noreply.github.com>
2020-05-14 23:27:48 +0700

    Kategori No Induk

---
+++ v_kategori_no_induk_edit.php
@@ -0,0 +1,53 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?php
+    $row = fetch_single_row($edit);
+?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Kategori No Induk</label>
+            <div class="col-sm-8">
+            <?php echo form_hidden('id',$row->id); ?>
+            <?php echo form_input(array('name'=>'master_kategori_alat_dan_bahan','value'=>$row->no_induk,'class'=>'form-control'));?>
+            <?php echo form_error('master_kategori_alat_dan_bahan');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Nama</label>
+            <div class="col-sm-8">
+            <?php echo form_hidden('id',$row->id); ?>
+            <?php echo form_input(array('name'=>'master_kategori_alat_dan_bahan','value'=>$row->nama,'class'=>'form-control'));?>
+            <?php echo form_error('master_kategori_alat_dan_bahan');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Keterangan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'master_kategori_alat_dan_bahan','value'=>$row->keterangan,'class'=>'form-control'));?>
+            <?php echo form_error('master_kategori_alat_dan_bahan');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Save</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"master/kategori_no_induk/show_editForm/","#divsubcontent")','Simpan','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $(".select2").select2();
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+});
+</script>
