d32156d1e746af0a22fbf4a70c9e5af2d44a70e8
divakratya <64886481+divakratya@users.noreply.github.com>
2020-05-14 23:26:44 +0700

    Kategori No Induk

---
+++ v_kategori_no_induk_add.php
@@ -0,0 +1,51 @@
+\
+
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Kategori No Induk</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'no_induk','class'=>'form-control'));?>
+            <?php echo form_error('no_induk');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Nama</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'nama','class'=>'form-control'));?>
+            <?php echo form_error('nama');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Keterangan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'keterangan','class'=>'form-control'));?>
+            <?php echo form_error('keterangan');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Save</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"master/kategori_no_induk/show_addForm/","#divsubcontent")','Save','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+});
+</script>
