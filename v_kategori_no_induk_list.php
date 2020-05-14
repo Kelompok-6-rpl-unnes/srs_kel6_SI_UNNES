7a0cc4a57cdc6a8d1c4057cce82d291a6f0496ec
divakratya <64886481+divakratya@users.noreply.github.com>
2020-05-14 23:28:37 +0700

    Kategori No Induk

---
+++ v_kategori_no_induk_list.php
@@ -0,0 +1,64 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+      <div class="col-lg-12">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Master Kategori No Induk</h3>
+
+            <div class="box-tools pull-right">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
+                echo button('load_silent("master/kategori_no_induk/form/base","#modal")','Add New No Induk','btn btn-success');
+              } else {
+                # code...
+              }
+              ?>
+            </div>
+          </div>
+          <div class="box-body">
+            <table width="100%" id="tableku" class="table table-striped">
+              <thead>
+              <th>No</th>
+                <th>No Induk</th>
+                <th>Nama</th>
+                <th>Keterangan</th>
+                <th>Act</th>
+              </thead>
+              <tbody>
+          <?php 
+          $i = 1;
+          foreach($kategori_no_induk->result() as $row): ?>
+          <tr>
+            <td align="center"><?=$i++?></td>
+            <td align="center"><?=$row->no_induk?></td>
+            <td align="center"><?=$row->nama?></td>
+            <td align="center"><?=$row->keterangan?></td>
+            <td align="center">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
+                echo button('load_silent("master/kategori_no_induk/form/sub/'.$row->id.'","#modal")','','btn btn-info fa fa-edit','data-toggle="tooltip" title="Edit"');
+              } else {
+                # code...
+              }
+              ?>
+              <a href="<?= site_url('master/kategori_no_induk/delete/'.$row->id) ?>" class="btn btn-danger" onclick="return confirm('Anda yakin ingin menghapus alat ?')"><i class="fa fa-trash"></i></a>
+
+            </td>
+          </tr>
+
+        <?php endforeach;?>
+        </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    var table = $('#tableku').DataTable( {
+      "ordering": false,
+    } );
+  });
+</script>
