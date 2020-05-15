251b437332708b6301831a548c7aabf7072cb3e9
dimashardi13 <64641240+dimashardi13@users.noreply.github.com>
2020-05-15 10:44:34 +0700

    Add files via upload

---
+++ v_tipe_lab_add.php
@@ -0,0 +1,49 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Kode</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'kode','class'=>'form-control'));?>
+            <?php echo form_error('kode');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">tipe lab</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'tipe_lab','class'=>'form-control'));?>
+            <?php echo form_error('tipe_lab');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Keterangan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'keterangan','class'=>'form-control'));?>
+            <?php echo form_error('keterangan');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Simpan ya?</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"master/tipe_lab/show_addForm/","#divsubcontent")','SimpanLah','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+});
+</script>
