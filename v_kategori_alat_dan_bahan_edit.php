31b1103230303996c0631b7e5b46f36119d7c17f
RisaNailiFitriana <64651850+RisaNailiFitriana@users.noreply.github.com>
2020-05-15 15:32:42 +0700

    update v_kategori_alat_dan_bahan_edit

--- v_kategori_alat_dan_bahan_edit.php
+++ v_kategori_alat_dan_bahan_edit.php
@@ -10,16 +10,16 @@
             <label class="col-sm-4 control-label">Kategori Alat Bahan</label>
             <div class="col-sm-8">
             <?php echo form_hidden('id',$row->id); ?>
-            <?php echo form_input(array('name'=>'master_kategori_alat_dan_bahan','value'=>$row->Kategori,'class'=>'form-control'));?>
-            <?php echo form_error('master_kategori_alat_dan_bahan');?>
+            <?php echo form_input(array('name'=>'Kategori','value'=>$row->Kategori,'class'=>'form-control'));?>
+            <?php echo form_error('Kategori');?>
             <span id="check_data"></span>
             </div>
         </div>
         <div class="form-group">
             <label class="col-sm-4 control-label">Keterangan</label>
             <div class="col-sm-8">
-            <?php echo form_input(array('name'=>'master_kategori_alat_dan_bahan','value'=>$row->Keterangan,'class'=>'form-control'));?>
-            <?php echo form_error('master_kategori_alat_dan_bahan');?>
+            <?php echo form_input(array('name'=>'Keterangan','value'=>$row->Keterangan,'class'=>'form-control'));?>
+            <?php echo form_error('Keterangan');?>
             </div>
         </div>
         <div class="form-group">
