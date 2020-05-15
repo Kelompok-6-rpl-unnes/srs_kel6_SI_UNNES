da59a11f5d95a58aa29723cac8317d8cbf073731
dimashardi13 <64641240+dimashardi13@users.noreply.github.com>
2020-05-15 09:25:46 +0700

    Add files via upload

---
+++ v_tipe_lab_list.php
@@ -0,0 +1,61 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+    <div class="row" id="form_pembelian">
+      <div class="col-lg-12">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Master Tipe Lab</h3>
+
+            <div class="box-tools pull-right">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
+                echo button('load_silent("master/tipe_lab/form/base","#modal")','Add New Tipe_Lab','btn btn-success');
+              } else {
+                # code...
+              }
+              ?>
+            </div>
+          </div>
+          <div class="box-body">
+            <table width="100%" id="tableku" class="table table-striped">
+              <thead>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Keterangan</th>
+                <th>Act</th>
+              </thead>
+              <tbody>
+          <?php 
+          $i = 1;
+          foreach($satuan->result() as $row): ?>
+          <tr>
+            <td align="center"><?=$i++?></td>
+            <td align="center"><?=$row->nama?></td>
+            <td align="center"><?=$row->keterangan?></td>
+            <td align="center">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
+                echo button('load_silent("master/tipe_lab/form/sub/'.$row->id.'","#modal")','','btn btn-info fa fa-edit','data-toggle="tooltip" title="Edit"');
+              } else {
+                # code...
+              }
+              ?>
+            </td>
+          </tr>
+
+        <?php endforeach;?>
+        </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    var table = $('#tableku').DataTable( {
+      "ordering": false,
+    } );
+  });
+</script>
