91b8f0c882bf89fde6ca11f2cac362c46fc41a46
dimashardi13 <64641240+dimashardi13@users.noreply.github.com>
2020-05-15 10:37:45 +0700

    update tipe_lab

--- tipe_lab.php
+++ tipe_lab.php
@@ -1,8 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Nama_alat extends CI_Controller {
-
+class tipe_lab extends CI_Controller {
+	
 	public function __construct()
 	{
 		parent::__construct();
@@ -16,7 +16,6 @@ class Nama_alat extends CI_Controller {
 		$data['tipe_lab'] = $this->m_tipe_lab->getData();
 		$this->load->view('master/tipe_lab/v_tipe_lab_list',$data);
 	}
-
 	public function form($param='')
 	{
 		$content   = "<div id='divsubcontent'></div>";
@@ -31,7 +30,6 @@ class Nama_alat extends CI_Controller {
 			$this->fungsi->run_js('load_silent("master/tipe_lab/show_editForm/'.$base_kom.'","#divsubcontent")');	
 		}
 	}
-
 	public function show_addForm()
 	{
 		$this->fungsi->check_previleges('tipe_lab');
@@ -49,26 +47,25 @@ class Nama_alat extends CI_Controller {
 		if ($this->form_validation->run() == FALSE)
 		{
 			$data['status']='';
-			$this->load->view('master/tipe_lab/v_tipe_lab_list',$data);
+			$this->load->view('master/tipe_lab/v_tipe_lab_add',$data);
 		}
 		else
 		{
-			$datapost = get_post_data(array('kode','tipe_lab','keterangan','id_status'));
-			$this->m_nama_alat->insertData($datapost);
+			$datapost = get_post_data(array('id','kode','tipe_lab','keterangan','id_status'));
+			$this->m_tipe_lab->insertData($datapost);
 			$this->fungsi->run_js('load_silent("master/tipe_lab","#content")');
 			$this->fungsi->message_box("Data Master Tipe Lab sukses disimpan...","success");
 			$this->fungsi->catat($datapost,"Menambah Master tipe_lab dengan data sbb:",true);
 		}
 	}
-
 	public function show_editForm($id='')
 	{
 		$this->fungsi->check_previleges('tipe_lab');
 		$this->load->library('form_validation');
 		$config = array(
 				array(
-					'field'	=> 'id',
-					'label' => 'wes mbarke',
+					'field'	=> 'kode',
+					'label' => 'keterangan',
 					'rules' => ''
 				),
 				array(
@@ -88,14 +85,20 @@ class Nama_alat extends CI_Controller {
 		}
 		else
 		{
-			$datapost = get_post_data(array('id','kode','tipe_lab','keterangan','id_status'));
+			$datapost = get_post_data(array('id','kode','tipe_lab','keterangan','is_status'));
 			$this->m_tipe_lab->updateData($datapost);
 			$this->fungsi->run_js('load_silent("master/tipe_lab","#content")');
-			$this->fungsi->message_box("Data Master Tipe Lab sukses diperbarui...","success");
-			$this->fungsi->catat($datapost,"Mengedit Master tipe_labnama_alat dengan data sbb:",true);
+			$this->fungsi->message_box("Data Master Tipe Lab sukses disimpan...","success");
+			$this->fungsi->catat($datapost,"Menambah Master tipe_lab dengan data sbb:",true);
 		}
+	}
+	public function delete()
+	{
+		$id = $this->uri->segment(4);
+		$this->m_tipe_lab->deleteData($id);
+		redirect('admin');
 	}
 }
 
-/* End of file tipe_lab.php */
-/* Location: ./application/controllers/master/tipe_lab.php */
+
+	
