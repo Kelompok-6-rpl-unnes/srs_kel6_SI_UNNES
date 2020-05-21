ffaeab5a42780815487fb9d192a521f300b518e8
danendra-yassar <64626805+danendra-yassar@users.noreply.github.com>
2020-05-21 21:54:28 +0700

    view edit inven alat bahan

---
+++ v_inven_alat_bahan_edit.php
@@ -0,0 +1,57 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?php
+    $row = fetch_single_row($edit);
+?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Kode</label>
+            <div class="col-sm-8">
+            <?php echo form_hidden('id',$row->id); ?>
+            <?php echo form_input(array('name'=>'kode','value'=>$row->kode,'class'=>'form-control'));?>
+            <?php echo form_error('kode');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Nama Alat atau Bahan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'alat_bahan','value'=>$row->alat_bahan,'class'=>'form-control'));?>
+            <?php echo form_error('alat_bahan');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Tanggal</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'tanggal','value'=>$row->tanggal,'class'=>'form-control'));?>
+            <?php echo form_error('tanggal');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Keterangan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'keterangan','value'=>$row->keterangan,'class'=>'form-control'));?>
+            <?php echo form_error('keterangan');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Simpan</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"master/inven_alat_bahan/show_editForm/","#divsubcontent")','Simpan','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $(".select2").select2();
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+});
+</script>
