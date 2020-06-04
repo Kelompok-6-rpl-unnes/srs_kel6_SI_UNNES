58c0deef5be83a7465facbb0a32f2bbd503fb9ec
Erikanoordianti <erikanoordianti@students.unnes.ac.id>
2020-06-05 03:13:50 +0700

    update

---
+++ application/views/kelola/absen_tim_lab/v_absen_tim_lab_edit.php
@@ -0,0 +1,80 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?php
+    $row = fetch_single_row($edit);
+?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Nomor Induk</label>
+            <div class="col-sm-8">
+            <?php echo form_hidden('id',$row->id); ?>
+            <?php echo form_input(array('name'=>'nomor_induk','value'=>$row->nomor_induk,'class'=>'form-control'));?>
+            <?php echo form_error('nomor_induk');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Nama Lengkap</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'nama_lengkap','value'=>$row->nama_lengkap,'class'=>'form-control'));?>
+            <?php echo form_error('nama_lengkap');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Tanggal</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'tanggal','value'=>$row->tanggal,'class'=>'form-control'));?>
+            <?php echo form_error('tanggal');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Keterangan</label>
+            <input type="radio" name="keterangan" value="Masuk">Masuk<br/>
+            <div class="col-sm-8">
+            </div> 
+           <input type="radio" name="keterangan" value="Tidak Masuk">Tidak Masuk<br/> 
+           <div class="col-sm-4">
+           </div>
+            <input type="radio" name="keterangan" value="Ijin">Ijin<br/>
+            <div class="col-sm-8">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Jumlah Kehadiran</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'jumlah_kehadiran','value'=>$row->jumlah_kehadiran,'class'=>'form-control'));?>
+            <?php echo form_error('jumlah_kehadiran');?>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Jabatan/Profesi</label>
+            <select name ='jabatan'>
+          <div class="col-sm-8">
+          <option value ='Asisten Laboratorium'>Asisten Laboratorium</option>
+          <option value ='Ketua Laboratorium'>Ketua Laboratorium</option>
+          <option value ='Laboran'>Laboran</option>
+          <option value ='Koordinator Laboratorium'>Koordinator Laboratorium</option>
+          </select>
+          </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Simpan</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"kelola/absen_tim_lab/show_editForm/","#divsubcontent")','Simpan','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $(".select2").select2();
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+});
+</script>
---
+++ application/views/kelola/absen_tim_lab/v_absen_tim_lab_list.php
@@ -0,0 +1,81 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+    <div class="row" id="form_pembelian">
+      <div class="col-lg-12">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Kelola Absen Tim Laboratorium</h3>
+            </div>
+            </head>
+            <body>
+            <table>
+            <tr>
+            </div>
+            </div>
+            &emsp;
+            <div class="box-tools pull-right">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
+                echo button('load_silent("kelola/absen_tim_lab/form/base","#modal")','Add New Absen Tim Laboratorium','btn btn-success');
+              } else {
+                # code...
+              }
+              ?>
+            </div>
+          </div>
+         </div>
+          <div class="box-body">
+            <table width="100%" id="tableku" class="table table-striped">
+              <thead>
+                <th>ID</th>
+                <th>Nomor Induk</th>
+                <th>Nama Lengkap</th>
+                <th>Tanggal</th>
+                <th>Keterangan</th>
+                <th>Jumlah Kehadiran</th>
+                <th>Jabatan/Profesi</th>
+                <th>Act</th>
+              </thead>
+              <tbody>
+              <?php 
+          $i = 1;
+          foreach($absen_tim_lab->result() as $row): ?>
+          <tr>
+            <td align="center"><?=$i++?></td>
+            <td align="center"><?=$row->nomor_induk?></td>
+            <td align="center"><?=$row->nama_lengkap?></td>
+            <td align="center"><?=$row->tanggal?></td> 
+            <td align="center"><?=$row->keterangan?></td>
+            <td align="center"><?=$row->jumlah_kehadiran?></td>
+            <td align="center"><?=$row->jabatan?></td>
+            <td align="center">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '3' || $sesi == '6') {
+                echo button('load_silent("kelola/absen_tim_lab/form/sub/'.$row->id.'","#modal")','','btn btn-info fa fa-edit','data-toggle="tooltip" title="Edit"');
+              } else {
+                # code...
+              }
+              ?>
+              <a href="<?= site_url('kelola/absen_tim_lab/delete/'.$row->id) ?>" class="btn btn-danger" onclick="return confirm('Anda yakin ingin menghapus absen tim laboratorium?')"><i class="fa fa-trash"></i></a>
+            </td>
+          </tr>
+          <?php endforeach;?>
+        </tbody>
+        </table>
+        <a href="" class="btn btn-warning" style="margin-bottom;
+        10px; "title="view/utily/v_dashboard.php"></i>Back</a>
+        </div>
+       </div>
+      </div>
+      </div>
+      </div>
+      </div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    var table = $('#tableku').DataTable( {
+      "ordering": false,
+    } );
+  });
+</script>
