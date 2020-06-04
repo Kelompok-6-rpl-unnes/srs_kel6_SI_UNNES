273f73d2ad13ee1f34aee2739a3e8a2901cd6ea2
danendra-yassar <64626805+danendra-yassar@users.noreply.github.com>
2020-06-04 22:25:53 +0700

    add peminjaman

---
+++ application/views/peminjaman/v_pinjam_alat_bahan_add.php
@@ -0,0 +1,64 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+<div class="box-body big">
+    <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
+        
+        
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Kode</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'kode','class'=>'form-control'));?>
+            <?php echo form_error('kode');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Peminjaman Alat dan Bahan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'pinjam_alat_bahan','class'=>'form-control'));?>
+            <?php echo form_error('pinjam_alat_bahan');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">jumlah</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'jumlah','class'=>'form-control'));?>
+            <?php echo form_error('jumlah');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Tanggal</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'tanggal','class'=>'form-control'));?>
+            <?php echo form_error('tanggal');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Keterangan</label>
+            <div class="col-sm-8">
+            <?php echo form_input(array('name'=>'keterangan','class'=>'form-control'));?>
+            <?php echo form_error('keterangan');?>
+            <span id="check_data"></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-4 control-label">Save</label>
+            <div class="col-sm-8 tutup">
+            <?php
+            echo button('send_form(document.faddmenugrup,"master/pinjam_alat_bahan/show_addForm/","#divsubcontent")','Save','btn btn-success')." ";
+            ?>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.tutup').click(function(e) {  
+        $('#myModal').modal('hide');
+    });
+});
+</script>
