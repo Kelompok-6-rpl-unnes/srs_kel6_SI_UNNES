73d4d38dd321393efbc1e7859d53121852ce16dc
Erikanoordianti <erikanoordianti@gmail.com>
2020-06-16 09:52:35 +0700

    ganti dikit

--- assets/index.php
+++ assets/index.php
@@ -5,42 +5,42 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Sistem Laboratorium</title>
-    <link rel="shortcut icon" href="http://localhost/sitempe/assets/img/title.png" />
+    <link rel="shortcut icon" href="http://localhost/srs_kel6_SI_UNNES/assets/img/title.png" />
     <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.5 -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNESassets/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/css/ionicons.min.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/css/ionicons.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
   folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/css/skins/_all-skins.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/iCheck/flat/blue.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/iCheck/flat/blue.css">
   <!-- Morris chart -->
-  <!-- <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/morris/morris.css"> -->
+  <!-- <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/morris/morris.css"> -->
   <!-- jvectormap -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
   <!-- Date Picker -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/datepicker/datepicker3.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/datepicker/datepicker3.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/daterangepicker/daterangepicker-bs3.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/daterangepicker/daterangepicker-bs3.css">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <!-- GROWL NOTIFICATION -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/css/jquery.growl.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/css/jquery.growl.css">
   <!-- SELECT2 -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/css/select2.min.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/css/select2.min.css">
   <!-- DataTables -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/datatables/dataTables.bootstrap.css">
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/datatables/extensions/FixedColumns/css/dataTables.fixedColumns.min.css"></script>
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/datatables/extensions/TableTools/css/dataTables.tableTools.min.css"></script>
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/datatables/dataTables.bootstrap.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/datatables/extensions/FixedColumns/css/dataTables.fixedColumns.min.css"></script>
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/datatables/extensions/TableTools/css/dataTables.tableTools.min.css"></script>
   <!-- Theme style -->
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/css/AdminLTE.css">
-  <link rel="stylesheet" href="http://localhost/sitempe/assets/plugins/pace/pace.min.css">
-  <link href="http://localhost/sitempe/assets/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/css/AdminLTE.css">
+  <link rel="stylesheet" href="http://localhost/srs_kel6_SI_UNNES/assets/plugins/pace/pace.min.css">
+  <link href="http://localhost/srs_kel6_SI_UNNES/assets/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
   </head>
   <body class="hold-transition skin-purple-light sidebar-mini fixed">
   <style type="text/css">
@@ -62,8 +62,8 @@
         <!-- Logo -->
         <a href="#" class="logo">
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-mini">SITEMPE</span>
-          <span class="logo-lg">SITEMPE</span>
+          <span class="logo-mini">EDUCATION LABORATORIUM SYSTEM</span>
+          <span class="logo-lg">EDUCATION LABORATORIUM SYSTEM</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
@@ -74,7 +74,7 @@
           <div class="navbar-custom-menu" >
             <ul class="nav navbar-nav">
               <li class="dropdown tasks-menu">
-                <a href="http://localhost/sitempe/login/logout/">
+                <a href="http://localhost/srs_kel6_SI_UNNES/login/logout/">
                   <i class="fa fa-fw fa-power-off"></i>&nbsp;
                   Logout </a>
               </li>             
@@ -89,7 +89,7 @@
           <!-- Sidebar user panel -->
           <div class="user-panel">
             <div class="pull-left image">
-                            <img src="http://localhost/sitempe/assets/img/avatar04.png" alt="Kholiq" title="Kholiq" width="40" height="40" class=""/>            </div>
+                            <img src="http://localhost/srs_kel6_SI_UNNES/assets/img/avatar04.png" alt="Kholiq" title="Kholiq" width="40" height="40" class=""/>            </div>
             <div class="pull-left info">
               <p><font color="#bfbfbf" size="2px">Login As :</font></p>
               <p>Kholiq</p>
@@ -200,34 +200,34 @@
 
  <!-- jQuery 2.1.4 -->
  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-    <script type="text/javascript" src="http://localhost/sitempe/assets/js/jquery-ui.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/pace/pace.min.js"></script>    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/js/jquery-ui.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/pace/pace.min.js"></script>    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <!-- Bootstrap 3.3.5 -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/js/bootstrap.min.js"></script>
     <!-- Select2 -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/select2/select2.full.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/fileinput.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/ajaxFileUpload.js"></script>    <!-- Morris.js charts -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/select2/select2.full.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/fileinput.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/ajaxFileUpload.js"></script>    <!-- Morris.js charts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
     <!-- Sparkline -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/sparkline/jquery.sparkline.min.js"></script>    <!-- jvectormap -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js"></script>    <!-- jQuery Knob Chart -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/knob/jquery.knob.js"></script>    <!-- daterangepicker -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/js/moment.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/ckeditor.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/daterangepicker/daterangepicker.js"></script>    <!-- datepicker -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/datepicker/bootstrap-datepicker.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/autocomplete/jquery.autocomplete.js"></script>    <!-- Bootstrap WYSIHTML5 -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>    <!-- Slimscroll -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/slimScroll/jquery.slimscroll.min.js"></script>    <!-- FastClick -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/fastclick/fastclick.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>    <!-- AdminLTE App -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/js/jquery.blockUI.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/app.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/bootapp_back.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/fullcalendar/fullcalendar.min.js"></script>
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/sparkline/jquery.sparkline.min.js"></script>    <!-- jvectormap -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js"></script>    <!-- jQuery Knob Chart -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/knob/jquery.knob.js"></script>    <!-- daterangepicker -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/js/moment.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/ckeditor.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/daterangepicker/daterangepicker.js"></script>    <!-- datepicker -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/datepicker/bootstrap-datepicker.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/autocomplete/jquery.autocomplete.js"></script>    <!-- Bootstrap WYSIHTML5 -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>    <!-- Slimscroll -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/slimScroll/jquery.slimscroll.min.js"></script>    <!-- FastClick -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/fastclick/fastclick.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>    <!-- AdminLTE App -->
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/js/jquery.blockUI.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/app.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/bootapp_back.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/fullcalendar/fullcalendar.min.js"></script>
     <!-- GROWL NOTIFICATION -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/js/jquery.growl.js"></script>
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/js/jquery.growl.js"></script>
 
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/input-mask/jquery.inputmask.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/input-mask/jquery.inputmask.date.extensions.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/input-mask/jquery.inputmask.extensions.js"></script>
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/input-mask/jquery.inputmask.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/input-mask/jquery.inputmask.date.extensions.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/input-mask/jquery.inputmask.extensions.js"></script>
     <!-- DataTables -->
-    <script type="text/javascript" src="http://localhost/sitempe/assets/plugins/datatables/jquery.dataTables.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/datatables/dataTables.bootstrap.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/datatables/extensions/FixedColumns/js/dataTables.fixedColumns.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/dataTables.fixedColumns.min.js"></script>
+    <script type="text/javascript" src="http://localhost/srs_kel6_SI_UNNES/assets/plugins/datatables/jquery.dataTables.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/datatables/dataTables.bootstrap.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/datatables/extensions/FixedColumns/js/dataTables.fixedColumns.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js"></script>    <script type="text/javascript" src="http://localhost/laboratorium_siladu/assets/js/dataTables.fixedColumns.min.js"></script>
     <script>
     $(document).ajaxStart(function() { Pace.restart(); });
-    var site = 'http://localhost/sitempe/';
-    var base = 'http://localhost/sitempe/';
-    var gbr1 = 'http://localhost/sitempe/assets/img/loaders/biru.jpg'
-    var gbr2 = 'http://localhost/sitempe/assets/img/loaders/4.gif'
+    var site = 'http://localhost/srs_kel6_SI_UNNES/';
+    var base = 'http://localhost/srs_kel6_SI_UNNES/';
+    var gbr1 = 'http://localhost/srs_kel6_SI_UNNES/assets/img/loaders/biru.jpg'
+    var gbr2 = 'http://localhost/srs_kel6_SI_UNNES/assets/img/loaders/4.gif'
       $.widget.bridge('uibutton', $.ui.button);
     $(function() {
       $('#Dashboard').click();
