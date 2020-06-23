85fe7d43f9fd4470ba8711565f337da5f1fd01d3
divakratya <dkratya@students.unnes.ac.id>
2020-06-23 20:28:44 +0700

    seri alat

---
+++ application/views/kelola/seri_alat/seri_alat_list.php
@@ -0,0 +1,101 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+
+    <div class="row" id="form_pembelian">
+      <div class="col-lg-12">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Kelola Seri Alat</h3>
+            </div>
+            </head>
+            <body>
+            <table>
+            <tr>
+            </div>
+            </div>
+            &emsp;
+            <div class="box-tools pull-right">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '7') {
+                echo button('load_silent("kelola/seri_alat/form/base","#modal")','Add New Seri Alat','btn btn-success');
+              } else {
+                # code...
+              }
+              ?>
+            </div>
+          </div>
+         </div>
+          <div class="box-body">
+            <table width="100%" id="tableku" class="table table-striped">
+              <thead>
+                <th>ID</th>
+                <th>Kategori</th>
+                <th>Merk</th>
+                <th>Seri</th>
+                <th>Gambar</th>
+                <th>SOP Pengoperasian</th>
+                <th>SOP Pemeliharaan</th>
+                <th>SOP Pemeriksaan</th>
+                <th>SOP Kalibrasi</th>
+                <th>SOP Uji Fungsi</th>
+                <th>Stok</th>
+                <th>Stok Minimal</th>
+                <th>Status</th>
+                <th>Act</th>
+              </thead>
+              <tbody>
+              <?php 
+          $i = 1;
+          foreach($seri_alat->result() as $row): ?>
+          <tr>
+            <td align="center"><?=$i++?></td>
+            <td align="center"><?=$row->katerogi?></td>
+            <td align="center"><?=$row->merk?></td>
+            <td align="center"><?=$row->seri?></td> 
+            <td align="center"><img src="<?php echo base_url().$row->gambar; ?>" class="file-preview-image"></td>
+            <td align="center"><?=$row->sop_pengoperasian?></td>
+            <td align="center"><?=$row->sop_pemeliharaan?></td>
+            <td align="center"><?=$row->sop_pemeriksaan?></td>
+            <td align="center"><?=$row->sop_kalibrasi?></td>
+            <td align="center"><?=$row->sop_uji_fungsi?></td>
+            <td align="center"><?=$row->stok?></td>
+            <td align="center"><?=$row->stok_minimali?></td>
+            <td align="center"><?=$row->sop_kalibrasi?></td>
+            <td align="center"><span class="badge bg-green"><?=$row->status?></td>
+            <td align="center">
+            <?php
+              $sesi = from_session('level');
+              if ($sesi == '1' || $sesi == '2' || $sesi == '7') {
+                echo button('load_silent("kelola/seri_alat/form/sub/'.$row->id.'","#modal")','','btn btn-info fa fa-edit','data-toggle="tooltip" title="Edit"');
+              } else {
+                # code...
+              }
+              ?>
+              <?php
+            $sesi = from_session('level');
+            if ($sesi == '1' || $sesi == '2'|| $sesi == '7') {
+             echo button('load_silent("kelola/seri_alat/delete/'.$row->id.'","#content")','','btn btn-danger fa fw fa-trash','data-toggle="tooltip" title="Hapus"');
+            } else {
+              # code...
+            }
+             ?>   
+            </td>
+          </tr>
+          <?php endforeach;?>
+        </tbody>
+        </table>
+        <a href="" class="btn btn-warning" style="margin-bottom;
+        10px; "title="view/utily/v_dashboard.php"></i>Back</a>
+        </div>
+       </div>
+      </div>
+      </div>
+      </div>
+      </div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    var table = $('#tableku').DataTable( {
+      "ordering": false,
+    } );
+  });
+</script>
