bb98b2865cf73f96bb2ba58fb4fa98c54a0fcfb4
Erikanoordianti <erikanoordianti@gmail.com>
2020-06-27 13:13:52 +0700

    database

---
+++ database/master_sumber_pendanaan.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 27, 2020 at 08:10 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sionlab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_sumber_pendanaan`
+--
+
+CREATE TABLE `master_sumber_pendanaan` (
+  `id` int(11) NOT NULL,
+  `sumber_pendanaan` varchar(255) NOT NULL,
+  `sejumlah` int(100) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `master_sumber_pendanaan`
+--
+
+INSERT INTO `master_sumber_pendanaan` (`id`, `sumber_pendanaan`, `sejumlah`, `keterangan`) VALUES
+(1, 'Fakultas', 2000000, 'Sudah Masuk');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `master_sumber_pendanaan`
+--
+ALTER TABLE `master_sumber_pendanaan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `master_sumber_pendanaan`
+--
+ALTER TABLE `master_sumber_pendanaan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
