811e31276303d0c34a6d3b97d49a363340130b38
RisaNailiFitriana <risanaili@students.unnes.ac.id>
2020-07-01 15:22:33 +0700

    naruh database

--- database/peminjaman.sql
+++ database/peminjaman.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2020 at 09:29 AM
+-- Generation Time: Jul 01, 2020 at 09:32 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
